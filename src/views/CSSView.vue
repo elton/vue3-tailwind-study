ff89f94e7d3b0882ab2042568d3f3850234007ef
Elton Zheng <elton.zheng.sh@gmail.com>
2025-02-12 10:52:05 +0800

    update css on css vue component

--- src/views/CSSView.vue
+++ src/views/CSSView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-5xl font-bold underline text-teal-800">TailWindCSS</div>
+  <div class="text-5xl font-bold underline text-emerald-800">TailWindCSS</div>
 </template>
 
 <script lang="ts">
